ac7bf665571a581487c82314f2adbe1babb53ee3
Sergey Shcherbina <bshbfg@gmail.com>
2022-02-07 20:46:19 +0500

    added prisma and next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth/next'
+import EmailProvider from 'next-auth/providers/email'
+import GitHubProvider from 'next-auth/providers/github'
+
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+})
